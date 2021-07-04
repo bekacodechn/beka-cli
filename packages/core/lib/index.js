ec5d1af5a3efb62af24f3da02bd915b692bc324c
w_shilinjun <bekacodechn@hotmail.com>
2021-07-04 15:42:38 +0800

    feat: 第一次提交

---
+++ packages/core/lib/index.js
@@ -0,0 +1,9 @@
+'use strict';
+const util = require('@bekacode/util')
+
+module.exports = core;
+function core() {
+    util()
+}
+
+core()
---
+++ packages/util/lib/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = util;
+
+function util() {
+    console.log('util-util')
+}
